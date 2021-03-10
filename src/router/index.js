89f63d048b4c5c9b4200a519f7f01f753f5619bb
DESKTOP-BS8VU1A\NXUSER <han0910812144@gmail.com>
2021-03-10 11:01:33 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import CRUD from '../views/CRUD.vue'
 Vue.use(VueRouter)
 
-<<<<<<< HEAD
 const routes = [{
     path: '/',
     name: 'Home',
@@ -15,24 +13,6 @@ const routes = [{
     name: 'login',
     component: () => import( /* webpackChunkName: "about" */ '../views/login.vue')
   }
-=======
-const routes = [
-    {
-        path: '/crud',
-        name: 'CRUD',
-        component: CRUD,
-    },
-    {
-        path: '/',
-        name: 'Home',
-        component: Home,
-    },
-    {
-        path: '/about',
-        name: 'About',
-        component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
-    }
->>>>>>> origin/main
 ]
 
 const router = new VueRouter({
